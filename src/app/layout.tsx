0d09248f822adc14d36b30b190b74063b6b2b7ee
Jake Ferrero <jake.a.ferrero@gmail.com>
2024-07-08 16:56:02 -0400

    Remove comments and update nextjs config basePath

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,26 +5,6 @@ import Sidebar from './components/sidebar';
 import './globals.css';
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
-  // left side, 3 verticle buttons - home, about, contact
-  // home is just: name, title, about me and pic, download link with resume
-  // contact is an email div
-
-  // on exp page, top is skills, bottom is half exp, hald edu
-  // for skills part, list where each li is a one word header, then under that is
-  // sentence describing.
-  // Ex.
-  // AWS
-  // Extensive experience developing distributed, cloud-based microservices leveraging many
-  // of AWS's services
-  // NodeJS + Typescript
-  // AWS
-  // Leadership
-  // CICD??
-  // React
-
-  // on exp page, for exp section
-  // 2 divs side by side - if page is to short, stack them
-  // each element is a COMPONENT - resumeComponent
   return (
     <html lang="en">
       <body>
